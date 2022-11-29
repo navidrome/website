e67d5b04f97da1798f2250b11887fd7b92997c1b
Deluan Quintão <deluan@deluan.com>
2022-11-29 14:40:27 -0500

    Restructure images folder (#101)

--- sync-images.sh
+++ sync-images.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-mkdir -p static/backgrounds
-rm -rf static/backgrounds/*
+set -e
 
-for i in `cat static/images`; do
+mkdir -p static/images
+rm -rf static/images/*
+
+echo "---" > static/images/index.yml
+for i in `cat background_images.txt`; do
     url="https://unsplash.com/photos/${i}/download?fm=jpg&w=1600&h=900&fit=max"
     echo $url
-    curl -sSL $url > static/backgrounds/${i}.jpg
+    curl -sSL $url > static/images/${i}.jpg
+    echo "- ${i}.jpg" >> static/images/index.yml
 done
